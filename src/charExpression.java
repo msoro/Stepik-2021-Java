a4dec7a7363e721caf7e0879b6aaf382d8e31b36
Maxim Sorokin <maxim.sorokin@gmail.com>
2021-07-08 12:36:40 +0300

    added class charExpression

--- src/charExpression.java
+++ src/charExpression.java
@@ -2,6 +2,8 @@
   Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\"
   (обратный слэш) на расстоянии a.
 
+  Stepik code:
+
    public static char charExpression(int a) {
     int ch = '\\';
     int x = ch + a;
