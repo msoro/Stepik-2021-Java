20b72603286c2aa70a649e667902303bc98c66f3
Maxim Sorokin <maxim.sorokin@gmail.com>
2021-07-08 12:34:58 +0300

    Initial commit

---
+++ src/charExpression.java
@@ -0,0 +1,26 @@
+/*
+  Реализуйте метод, который возвращает букву, стоящую в таблице UNICODE после символа "\"
+  (обратный слэш) на расстоянии a.
+
+   public static char charExpression(int a) {
+    int ch = '\\';
+    int x = ch + a;
+    char unicode = (char)x;
+    return unicode;
+}
+ */
+
+import java.util.Scanner;
+
+public class charExpression {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a);
+        int ch = '\\';
+        int x = ch + a;
+        char unicode = (char)x;
+        System.out.println(unicode);
+    }
+}
+
